01ed0f22dedc24f762c21e6cc634d0439ca1b04d
sashman <sashman90@gmail.com>
2019-08-21 20:32:08 +0100

    Refactoring test

--- spec/checkout_spec.rb
+++ spec/checkout_spec.rb
@@ -1,19 +1,22 @@
 RSpec.describe RubyBilling::Checkout do
   describe ".total" do
-    it "returns 0 for no items" do
-      co = RubyBilling::Checkout.new(nil)
+    context "without promotions" do
+      before do
+        @co = RubyBilling::Checkout.new(nil)
+      end
 
-      expect(co.total).to eq(0)
-    end
+      it "returns 0 for no items" do
+        expect(@co.total).to eq(0)
+      end
 
-    it "returns total price for multiple items w/o promotions" do
-      co = RubyBilling::Checkout.new(nil)
-      item = RubyBilling::Item.new("001", "test", 100)
-      co.scan(item)
-      co.scan(item)
-      co.scan(item)
+      it "returns total price for multiple items" do
+        item = RubyBilling::Item.new("001", "test", 100)
+        @co.scan(item)
+        @co.scan(item)
+        @co.scan(item)
 
-      expect(co.total).to eq(Money.new(300))
+        expect(@co.total).to eq(Money.new(300))
+      end
     end
   end
 end

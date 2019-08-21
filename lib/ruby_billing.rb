cc395452bf575da96681de6732f82ec39344b76b
sashman <sashman90@gmail.com>
2019-08-21 19:32:24 +0100

    Initial commit

---
+++ lib/ruby_billing.rb
@@ -0,0 +1,6 @@
+require "ruby_billing/version"
+
+module RubyBilling
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/ruby_billing/version.rb
@@ -0,0 +1,3 @@
+module RubyBilling
+  VERSION = "0.1.0"
+end
